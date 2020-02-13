0159fbe2e601f1e3dab7e4305daa6815308dc308
Haruka-W <nekonoko1995@gmail.com>
2020-02-13 17:46:29 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '032e98bd8dd74e2f3da3b3fce36a2b54f9eace39c639ee25f91e49cf0f71281f22a688deb645298644cbfd81e07dfe413f6647044311e7bf7b0430120f869a1f'
